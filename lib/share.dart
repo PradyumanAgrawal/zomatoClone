b5b767c4e3d0cc4cc58aa93c5240bb554a790140
pranavbaradkar <pyb11@iitbbs.ac.in>
2020-04-21 02:20:19 +0530

    signUp page

--- lib/share.dart
+++ lib/share.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
+
 class Share extends StatelessWidget {
+   
+      
   @override
   Widget build(BuildContext context) {
-    return Text('This is Share Screen');
+    return SafeArea(
+      child: Scaffold(
+        body: Text("This is share"),
+      ),
+    );
   }
 }
