f8e516e3cf958496581898ba9760bc595af3f90f
Murtazav <mhv11@iitbbs.ac.in>
2020-04-20 16:31:26 +0530

    Appbar menu drawer issue solved everything converted to sliver

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -15,104 +15,164 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: DrawerWidget(),
-      body: ListView(
-        children: <Widget>[
-          _top(),
-          SizedBox(height: 5.0),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Super Offers",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            floating: true,
+            pinned: false,
+            snap: true,
+            backgroundColor: Colors.deepPurple[800],
+            title: Text(
+              "Porsio",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            actions: <Widget>[
+              IconButton(
+                  icon: Icon(
+                    Icons.notifications,
+                    color: Colors.white,
+                  ),
+                  onPressed: null)
+            ],
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30))),
+            bottom: PreferredSize(
+              child: Container(
+                padding: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20)),
                 ),
-                Text(
-                  "View All",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search",
+                    fillColor: Colors.white,
+                    filled: true,
+                    suffixIcon: Icon(Icons.search),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(color: Colors.transparent),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 16.0,
+                    ),
+                  ),
                 ),
-              ],
+              ),
+              preferredSize: Size(50, 80),
             ),
           ),
-          Container(
-            height: 200.0,
-            child: Carousel(
-              boxFit: BoxFit.cover,
-              images: [
-                Image.asset("assets/images/LOGO2.png"),
-                Image.asset("assets/images/LOGO2.png"),
-                Image.asset("assets/images/LOGO2.png"),
-                Image.asset("assets/images/LOGO2.png"),
-                Image.asset("assets/images/LOGO2.png"),
-              ],
-              autoplay: true,
-              dotSize: 5.0,
-              dotColor: Colors.black,
-              dotIncreasedColor: Colors.red,
-              dotBgColor: Colors.transparent,
-              indicatorBgPadding: 2.0,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Products",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          SliverList(
+              delegate: SliverChildListDelegate(
+            <Widget>[
+              SizedBox(height: 5.0),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Super Offers",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    Text(
+                      "View All",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 12.0),
+                    ),
+                  ],
                 ),
-                Text(
-                  "Explore",
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12.0),
+              ),
+              Container(
+                height: 200.0,
+                child: Carousel(
+                  boxFit: BoxFit.cover,
+                  images: [
+                    Image.asset("assets/images/LOGO2.png"),
+                    Image.asset("assets/images/LOGO2.png"),
+                    Image.asset("assets/images/LOGO2.png"),
+                    Image.asset("assets/images/LOGO2.png"),
+                    Image.asset("assets/images/LOGO2.png"),
+                  ],
+                  autoplay: true,
+                  dotSize: 5.0,
+                  dotColor: Colors.black,
+                  dotIncreasedColor: Colors.red,
+                  dotBgColor: Colors.transparent,
+                  indicatorBgPadding: 2.0,
                 ),
-              ],
-            ),
-          ),
-          SizedBox(height: 10.0),
-          Container(
-            child: Row(
-              children: <Widget>[
-                _singleProd("item 1"),
-                _singleProd("item 2"),
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                _singleProd("item 1"),
-                _singleProd("item 2"),
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                _singleProd("item 1"),
-                _singleProd("item 2"),
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                _singleProd("item 1"),
-                _singleProd("item 2"),
-              ],
-            ),
-          ),
-          Container(
-            child: Row(
-              children: <Widget>[
-                _singleProd("item 1"),
-                _singleProd("item 2"),
-              ],
-            ),
-          ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "Products",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    Text(
+                      "Explore",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10.0),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    _singleProd("item 1"),
+                    _singleProd("item 2"),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    _singleProd("item 1"),
+                    _singleProd("item 2"),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    _singleProd("item 1"),
+                    _singleProd("item 2"),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    _singleProd("item 1"),
+                    _singleProd("item 2"),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    _singleProd("item 1"),
+                    _singleProd("item 2"),
+                  ],
+                ),
+              ),
+            ],
+          )),
         ],
       ),
     );
@@ -135,58 +195,6 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         Text(name),
       ],
-    );
-  }
-
-  _top() {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        color: Colors.deepPurple[800],
-        borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  SizedBox(width: 10.0),
-                  Text(
-                    "Porsio",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-              IconButton(
-                  icon: Icon(Icons.notifications, color: Colors.white),
-                  onPressed: null),
-            ],
-          ),
-          SizedBox(height: 20.0),
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Search",
-              fillColor: Colors.white,
-              filled: true,
-              suffixIcon: Icon(Icons.search),
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20.0),
-                borderSide: BorderSide(color: Colors.transparent),
-              ),
-              contentPadding: EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 16.0,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
