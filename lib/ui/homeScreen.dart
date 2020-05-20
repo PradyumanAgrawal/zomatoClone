6f0707eb2d0029468c361bb1888e13acc34a9d54
swapnil1023 <50006812+swapnil1023@users.noreply.github.com>
2020-05-20 17:30:47 +0530

    resolved location filter

--- lib/ui/homeScreen.dart
+++ lib/ui/homeScreen.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:my_flutter_app/functionalities/firestore_service.dart';
-import 'package:my_flutter_app/functionalities/local_data.dart';
-import 'package:my_flutter_app/functionalities/location_service.dart';
 import './drawerWidget.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'filter_location.dart';
 
 class HomeScreen extends StatefulWidget {
   BuildContext navContext;
@@ -15,6 +13,11 @@ class HomeScreen extends StatefulWidget {
     this.add = add;
     this.navContext = navContext;
   } //: super(key: key);
+
+  void setAdd(String address) {
+    this.add = address;
+    HomeScreenState().rebuild();
+  }
 
   @override
   HomeScreenState createState() => HomeScreenState();
@@ -29,6 +32,10 @@ class HomeScreenState extends State<HomeScreen> {
     'assets/iphone11.jpg',
     'assets/laptop.jpg'
   ];
+
+  void rebuild() {
+    setState(() {});
+  }
 
   void _showAlertDialog(context) {
     showDialog(
@@ -70,102 +77,6 @@ class HomeScreenState extends State<HomeScreen> {
         });
   }
 
-  void _locationDialog(context) {
-    int centerx = 0, centery = 0;
-    GoogleMapController mapController;
-    showDialog(
-        context: context,
-        barrierDismissible: false,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            clipBehavior: Clip.hardEdge,
-            contentPadding: EdgeInsets.all(0),
-            insetPadding: EdgeInsets.all(0),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20.0)),
-            backgroundColor: Colors.white,
-            titleTextStyle: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black),
-            content: FutureBuilder(
-              future: LocalData().getLocation(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData) {
-                  var campos = CameraPosition(
-                    target: snapshot.data,
-                    zoom: 15,
-                  );
-                  return Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      GoogleMap(
-                        rotateGesturesEnabled: true,
-                        initialCameraPosition: campos,
-                        onMapCreated: (controller) {
-                          mapController = controller;
-                          mapController
-                              .getScreenCoordinate(snapshot.data)
-                              .then((value) {
-                            setState(() {
-                              centerx = value.x;
-                              centery = value.y;
-                            });
-                          });
-                        },
-                      ),
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.deepPurple,
-                        size: 50,
-                      ),
-                    ],
-                  );
-                } else {
-                  return Center(
-                    child: SpinKitChasingDots(
-                      color: Colors.purple,
-                    ),
-                  );
-                }
-              },
-            ),
-            actions: <Widget>[
-              ActionChip(
-                backgroundColor: Colors.deepPurple,
-                label: Padding(
-                  padding: const EdgeInsets.all(4.0),
-                  child: Text(
-                    'Set Location',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                onPressed: () {
-                  mapController
-                      .getLatLng(ScreenCoordinate(x: centerx, y: centery))
-                      .then((loc) {
-                    print(loc);
-                    LocationService().getAddress(loc).then((addrs) {
-                      setState(() {
-                        widget.add = addrs;
-                      });
-                    });
-                  });
-                },
-              ),
-              Material(
-                child: IconButton(
-                    icon:
-                        Icon(Icons.close, color: Colors.deepPurple, size: 30.0),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    }),
-              )
-            ],
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -186,17 +97,26 @@ class HomeScreenState extends State<HomeScreen> {
             ),
             actions: <Widget>[
               widget.add == ''
-                  ? Container()
-                  : ActionChip(
-                      backgroundColor: Colors.white,
-                      label: Text(
-                          widget.add.replaceAll(' ', '').substring(0, 10) +
-                              '...',
-                          style: TextStyle(color: Colors.deepPurple)),
+                  ? Icon(Icons.location_off)
+                  : IconButton(
+                      icon: Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
                       onPressed: () {
                         _showAlertDialog(context);
                       },
                     ),
+              // : ActionChip(
+              //     backgroundColor: Colors.white,
+              //     label: Text(
+              //         widget.add.replaceAll(' ', '').substring(0, 15) +
+              //             '...',
+              //         style: TextStyle(color: Colors.deepPurple)),
+              //     onPressed: () {
+              //       _showAlertDialog(context);
+              //     },
+              //   ),
               IconButton(
                 icon: Icon(
                   Icons.notifications,
@@ -253,10 +173,9 @@ class HomeScreenState extends State<HomeScreen> {
                     ),
                     OutlineButton(
                       onPressed: () {
-                        _locationDialog(context);
-                        /* Navigator.of(widget.navContext).pushNamed(
-                            '/filter_location',
-                            arguments: widget.add); */
+                        //Navigator.of(widget.navContext).pushNamed('/filter_location', arguments: widget.add);
+                        _awaitReturnValueFromFilter(
+                            widget.navContext, widget.add);
                       },
                       shape: StadiumBorder(),
                       splashColor: Colors.purple,
@@ -410,25 +329,25 @@ class HomeScreenState extends State<HomeScreen> {
                   },
                 ),
               ),
-              // Column(
-              //   children: List.generate(7, (index) {
-              //     return Padding(
-              //       padding: EdgeInsets.only(top: 5.0, bottom: 5.0),
-              //       child: Row(
-              //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              //         children: <Widget>[
-              //           _singleProd("Index $index", ''),
-              //           _singleProd("Index $index", ''),
-              //         ],
-              //       ),
-              //     );
-              //   }),
-              // ),
             ],
           )),
         ],
       ),
     );
+  }
+
+  void _awaitReturnValueFromFilter(BuildContext context, String add) async {
+    final result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FilterLocation(
+          add: add,
+        ),
+      ),
+    );
+    setState(() {
+      widget.add = result;
+    });
   }
 
   _singleProd(name, DocumentSnapshot document, BuildContext navContext) {
