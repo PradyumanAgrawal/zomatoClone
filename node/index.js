ef3936b8231ecf4023d3d16e4b4b73b07556f413
PradyumanAgrawal <pa14@iitbbs.ac.in>
2021-04-20 03:13:22 +0530

    basic queries implemented

--- node/index.js
+++ node/index.js
@@ -51,6 +51,17 @@ app.get('/user/:id', (req, res) => {
     });
 });
 
+//Insert user into database
+app.post('/user', (req, res) => {
+    con.connect(function(err) {
+        var sql = "INSERT INTO user VALUES (2,'Swapnil','ss93@iitbbs.ac.in','8085241233','Gondia','https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png')";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+        });
+    });
+});
+
 //Get all address for one specific user
 app.get('/address/:userId', (req, res) => {
     con.connect(function(err) {
@@ -66,22 +77,95 @@ app.get('/address/:userId', (req, res) => {
 app.delete('/address/:addrId', (req, res) => {
     con.connect(function(err) {
         console.log(req.params.id)
-        con.query(`SELECT * FROM address where userId=${req.params.id}`, function(err, result, fields) {
+        con.query(`DELETE FROM address where addrId=${req.params.addrId}`, function(err, result, fields) {
             if (err) res.send(err);
             if (result) res.send(result);
         });
     });
 });
 
-app.post('/user', (req, res) => {
+app.post('/address', (req, res) => {
     con.connect(function(err) {
-        var sql = "INSERT INTO user VALUES (2,'Swapnil','ss93@iitbbs.ac.in','8085241233','Gondia','https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png')";
+        var sql = "INSERT INTO address VALUES ";
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("1 record inserted");
         });
     });
 });
+
+//get all shops details
+app.get('/shops', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM shops`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+//get details for products for one shop
+app.get('/shops/:shopId', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM products  where shopId=${req.params.shopId}`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+//get all categories types
+app.get('/categories', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM categories`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+//Get all orders for one specific user
+app.get('/orders/:userId', (req, res) => {
+    con.connect(function(err) {
+        console.log(req.params.id)
+        con.query(`SELECT * FROM orders where userId=${req.params.userId}`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+//get all products details
+app.get('/products', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM products`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+//get all products details discount sorted
+app.get('/products', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM products orderBy discount`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+//get details for one product
+app.get('/products/:productId', (req, res) => {
+    con.connect(function(err) {
+        con.query(`SELECT * FROM products  where productId=${req.params.productId}`, function(err, result, fields) {
+            if (err) res.send(err);
+            if (result) res.send(result);
+        });
+    });
+});
+
+
 
 
 app.listen(port, () => {
